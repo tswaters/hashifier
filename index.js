4e38eba4544b7d219c071408d5ed95dd62d08f5e
Tyler Waters <tyler.waters@gmail.com>
2016-10-01 18:01:17 -0700

    Remove unnecessary semicolons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
+'use strict'
 
-var crypto = require('crypto');
+var crypto = require('crypto')
 
 /**
  * @typedef HashOptions
@@ -13,17 +13,17 @@ var crypto = require('crypto');
 /**
  * How many bytes to use for the hash.
  */
-var DEFAULT_SALT_LENGTH = 32;
+var DEFAULT_SALT_LENGTH = 32
 
 /**
  * Default number of iterations to perform.
  */
-var DEFAULT_ITERATIONS = 4096;
+var DEFAULT_ITERATIONS = 4096
 
 /**
  * Default algorithm to use.
  */
-var DEFAULT_ALGORITHM = 'sha512';
+var DEFAULT_ALGORITHM = 'sha512'
 
 /**
  * Hashes a value with salt.
@@ -34,31 +34,31 @@ var DEFAULT_ALGORITHM = 'sha512';
  * @throws exception if bad arguments provided.
  */
 function hash (plaintext /*, salt, options*/ ) {
-  var salt, options = {};
+  var salt, options = {}
   // figure out what parameters have been passed.
   switch (arguments.length) {
     case 1:
-      break;
+      break
     case 2:
       if (typeof arguments[1] === 'object') {
-        options = arguments[1];
+        options = arguments[1]
       }
       else {
-        salt = arguments[1];
+        salt = arguments[1]
       }
-      break;
+      break
     case 3:
-      salt = arguments[1];
-      options = arguments[2];
-      break;
+      salt = arguments[1]
+      options = arguments[2]
+      break
     default:
-      throw 'Invalid arguments provided.';
+      throw 'Invalid arguments provided.'
   }
 
   // load default options if unprovided or of the wrong type.
-  if (typeof options.iterations === 'undefined' || typeof options.iterations !== 'number' || options.iterations < 1) { options.iterations = DEFAULT_ITERATIONS; }
-  if (typeof options.algorithm === 'undefined' || typeof options.algorithm !== 'string') { options.algorithm = DEFAULT_ALGORITHM; }
-  if (typeof options.saltLength === 'undefined' || typeof options.saltLength !== 'number' || options.saltLength < 1) { options.saltLength = DEFAULT_SALT_LENGTH; }
+  if (typeof options.iterations === 'undefined' || typeof options.iterations !== 'number' || options.iterations < 1) { options.iterations = DEFAULT_ITERATIONS }
+  if (typeof options.algorithm === 'undefined' || typeof options.algorithm !== 'string') { options.algorithm = DEFAULT_ALGORITHM }
+  if (typeof options.saltLength === 'undefined' || typeof options.saltLength !== 'number' || options.saltLength < 1) { options.saltLength = DEFAULT_SALT_LENGTH }
 
   // console.info('got iterations %s', options.iterations)
   // console.info('got algorithm %s', options.algorithm)
@@ -66,20 +66,20 @@ function hash (plaintext /*, salt, options*/ ) {
 
   // ensure the specified algorithm is available to crypto.
   if (crypto.getHashes().indexOf(options.algorithm) === -1) {
-    throw 'Hashing algorithm ' + options.algorithm + ' is not available.';
+    throw 'Hashing algorithm ' + options.algorithm + ' is not available.'
   }
 
   // if has hasn't been provided, generate it.
   if (typeof salt === 'undefined' || typeof salt !== 'string') {
-    salt = crypto.randomBytes(options.saltLength).toString('hex');
+    salt = crypto.randomBytes(options.saltLength).toString('hex')
   }
 
   // generate the hash.
   for (var hashed = plaintext, i = 0, I = options.iterations; i < I; i++) {
-    hashed = crypto.createHmac(options.algorithm, salt).update(hashed).digest('hex');
+    hashed = crypto.createHmac(options.algorithm, salt).update(hashed).digest('hex')
   }
 
-  return [options.algorithm, salt, options.iterations, hashed].join('$');
+  return [options.algorithm, salt, options.iterations, hashed].join('$')
 }
 
 module.exports = {
@@ -91,7 +91,7 @@ module.exports = {
    * @returns {string}  salt;hash
    */
   hash: function (plaintext, options) {
-    return hash(plaintext, options);
+    return hash(plaintext, options)
   },
 
   /**
@@ -101,12 +101,12 @@ module.exports = {
    * @returns {boolean} whether the value matches
    */
   compare: function (plaintext, hashValue) {
-    var values = hashValue.split('$');
-    var salt = values[1];
+    var values = hashValue.split('$')
+    var salt = values[1]
     var options = {
       algorithm: values[0],
       iterations: values[2]
-    };
-    return hash(plaintext, salt, options) === hashValue;
+    }
+    return hash(plaintext, salt, options) === hashValue
   }
-};
+}
